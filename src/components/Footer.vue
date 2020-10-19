8dd8a4d0bb395e6ec38d6287aacda076ad4f92b2
Ove <ove.kristian.jorgensen@gmail.com>
2020-10-19 23:04:49 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,38 @@
 <template>
-  <div>
-      Footie
-  </div>
+  <v-footer
+    dark
+    padless
+    class="footah"
+  >
+    <v-card
+      flat
+      tile
+      class="lighten-1 white--text text-center foot-inner"
+    >
+      <!-- <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text> -->
+
+      <!-- <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text> -->
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        © {{ new Date().getFullYear() }} — <strong>Ove Jørgensen</strong>, All Rights Reserved
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
@@ -11,5 +42,10 @@ export default {
 </script>
 
 <style>
-
+.footah{
+    margin-top:100px
+}
+.foot-inner{
+    width: 100%;
+}
 </style>
